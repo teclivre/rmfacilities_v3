b78a47b3f7ca3a0e9b4f8563b4a1b23744b2cd86
teclivre <contato@portalsjcampos.com>
2026-08-11 20:56:56 -0300

    Corrige tema Material3 no app Android

--- rm-supervisor-android/app/build.gradle.kts
+++ rm-supervisor-android/app/build.gradle.kts
@@ -109,6 +109,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.google.android.material:material:1.12.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
