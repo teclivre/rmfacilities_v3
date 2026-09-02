8c43db938cd1699532a5d21770941563806dfbc2
teclivre <contato@portalsjcampos.com>
2026-09-02 08:35:19 -0300

    Renomeia aplicativo para RMFacilities Ponto

--- mobile-funcionario-android/app/src/main/java/br/com/rmfacilities/funcionarioapp/FcmService.kt
+++ mobile-funcionario-android/app/src/main/java/br/com/rmfacilities/funcionarioapp/FcmService.kt
@@ -62,7 +62,7 @@ class FcmService : FirebaseMessagingService() {
             "afastamento" -> "🏥 Afastamento Registrado"
             "pagamento" -> "💰 Pagamento Disponível"
             "ponto_lembrete" -> "⏰ Lembrete de Ponto"
-            else -> "RM Funcionário"
+            else -> "RMFacilities Ponto"
         }
         val corpo = message.notification?.body ?: data["corpo"] ?: when (tipo) {
             "documento_assinar" -> "Você tem um documento aguardando sua assinatura."
